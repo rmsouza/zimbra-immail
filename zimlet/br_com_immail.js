1390ad55bc1e75e8c173af99356088361cec2896
Rafael Menezes <rafaelmenezes86@gmail.com>
2019-03-18 11:42:28 -0400

    Fixed app instance

--- zimlet/br_com_immail.js
+++ zimlet/br_com_immail.js
@@ -23,11 +23,10 @@ var ZimbraImmailZimlet = br_com_immail_HandlerObject;
 * This method gets called by the Zimlet framework when the zimlet loads.
 */
 ZimbraImmailZimlet.prototype.init = function() {
-  this._simpleAppName = this.createApp(zimletInstance._zimletContext.getConfig("appName"), "zimbraIcon", zimletInstance._zimletContext.getConfig("appDescription"));
-
-  var app = appCtxt.getApp(appName); // get access to ZmZimletApp
   var zimletInstance = appCtxt._zimletMgr.getZimletByName('br_com_immail').handlerObject;
   zimletInstance.iframeURL = zimletInstance._zimletContext.getConfig("iframeURL");
+  zimletInstance.appName = zimletInstance._zimletContext.getConfig("appName");
+  zimletInstance.appDescription = zimletInstance._zimletContext.getConfig("appDescription");
 
   if (ZimbraImmailZimlet.prototype.sso()) {
     ZimbraImmailZimlet.prototype.loadIframe();
@@ -80,8 +79,20 @@ ZimbraImmailZimlet.prototype.sso = function() {
 };
 
 ZimbraImmailZimlet.prototype.loadIframe = function() {
-  var zimletInstance = appCtxt._zimletMgr.getZimletByName('br_com_immail').handlerObject;
-  var iframeURL = zimletInstance.iframeURL;
-  var token = zimletInstance.token;
-  app.setContent("<iframe id=\"tabiframe-app\" name=\"tabiframe-app\" src=\"" + iframeURL + "?token=" + token + "\" width=\"100%\" height=\"100%\" /></iframe>"); // write HTML to app
+  
+  try {
+    var zimletInstance = appCtxt._zimletMgr.getZimletByName('br_com_immail').handlerObject;
+    var iframeURL = zimletInstance.iframeURL;
+    var token = zimletInstance.token;
+
+    zimletInstance.ZimbraImmailApp = zimletInstance.createApp(zimletInstance.appName, "", zimletInstance.appDescription);
+    var app = appCtxt.getApp(zimletInstance.ZimbraImmailApp);
+
+    // var appPosition = document.getElementById('skin_container_app_new_button').getBoundingClientRect();
+    // app.setContent('<div style="position: fixed; top:'+appPosition.y+'px; left:0; width:100%; height:92%; border:0px;"><iframe id="ZimbraRocketFrame" style="z-index:2; left:0; width:100%; height:100%; border:0px;" src=\"'+zimletInstance._zimletContext.getConfig("rocketurl")+'\"></div>');
+
+    app.setContent("<iframe id=\"tabiframe-app\" name=\"tabiframe-app\" src=\"" + iframeURL + "?token=" + token + "\" width=\"100%\" height=\"100%\" /></iframe>"); // write HTML to app
+
+  } catch (err) { console.log (err)}
+
 };
